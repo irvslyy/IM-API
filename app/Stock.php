84e4e5230af8fc76357ef0b174fa21276339e912
hajidalakhtar <hajidalakhtar@gmail.com>
2020-07-21 09:05:56 +0700

    .

--- app/Stock.php
+++ app/Stock.php
@@ -1,14 +1,21 @@
 <?php
 
 namespace App;
-use Laravel\Passport\HasApiTokens; 
+
+use Laravel\Passport\HasApiTokens;
 use Illuminate\Database\Eloquent\Model;
 
 class Stock extends Model
 {
-    use Notifiable, HasApiTokens; 
-    
+    // use Notifiable, HasApiTokens; 
+
     protected $table = 'Stock';
     protected $connection = 'mysql_stock';
     public $timestamps = false;
+
+
+    public function item()
+    {
+        return $this->belongsTo('App\Items', 'items_code');
+    }
 }
