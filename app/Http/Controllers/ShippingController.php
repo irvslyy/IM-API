8a436dccb1e5f3d2ab7836821be77026d987db75
root <irvslyy23@gmail.com>
2020-08-24 10:51:26 +0700

    update shipping

--- app/Http/Controllers/ShippingController.php
+++ app/Http/Controllers/ShippingController.php
@@ -9,6 +9,11 @@ class ShippingController extends Controller
 {
     public function Shipping(Request $req)
     {
-        
+        $shipping = new Shipping;
+        $shipping->shipping_number = 'Shipping' . '-' . mt_rand(100,1000);
+        $shipping->user_id = 1;
+        $shipping->grf_number = $Goodreq->grf_number;
+        $shipping->req_code = $Goodreq->grf_number;
+        $shipping->save();
     }
 }
