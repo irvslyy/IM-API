101330f651e52058876a86e2fd55f903966a37c1
root <irvslyy23@gmail.com>
2020-08-28 17:41:53 +0700

    :white_check_mark:
    .

---
+++ app/Http/Middleware/validation.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+use Auth;
+use Closure;
+
+class validation
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (in_array($request->header->get('accept'))) {
+            return response()->json(['status' => 200]);
+        } else if(in_array($request->header->get('accept'))) {
+            return response()->json(['catch']);
+        }
+        
+
+        return $next($request);
+    }
+}
